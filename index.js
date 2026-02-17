065af153dc15846682f6a305902795b7a678c791
SrinidhiSwami <srinidhi.swami05@gmail.com>
2026-02-18 00:06:24 +0530

    final edit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         const targetElement = document.querySelector(targetId);
         
         if (targetElement) {
-            smoothScrollTo(targetElement, 1500);
+            smoothScrollTo(targetElement, 2500);
         }
     });
 });
@@ -38,9 +38,9 @@ function smoothScrollTo(element, duration) {
 
 function updateIcon() {
   if (document.body.classList.contains("dark")) {
-    toggleBtn.innerHTML = `<svg class="moon" class="w-4 h-4"  width="16px" height="16px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg> Light`;
+    toggleBtn.innerHTML = `<svg class="moon" class="w-4 h-4"  width="16px" height="16px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>  <span class="hide">Light</span>`;
   } else {
-    toggleBtn.innerHTML = `<svg class="moon" width="16px" height="16px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg> Dark`;
+    toggleBtn.innerHTML = `<svg class="moon" width="16px" height="16px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>  <span class="hide">Dark</span>`;
   }
 }
 
@@ -49,4 +49,36 @@ const toggleBtn = document.getElementById("theme-toggle");
 toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     updateIcon();
+    const theme = document.body.classList.contains("dark")
+    ? "dark" : "light";
+    localStorage.setItem("theme", theme);
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark");
+    }
+
+//     let all = document.getElementsByTagName("*");
+// for (let i = 0; i < all.length; i++) {
+//     if (all[i].offsetWidth > document.documentElement.offsetWidth + 1) {
+//         console.log(all[i]);
+//         all[i].style.border = "3px solid red";
+//     }
+// }
+});
+
+function openMenu() {
+    const menu = document.getElementById("menu");
+    menu.classList.add("active");
+    document.getElementById("menuBtn").style.display = "none";
+    document.getElementById("close").style.display = "flex";
+}
+
+function closeMenu() {
+    const menu = document.getElementById("menu");
+    menu.classList.remove("active");
+    document.getElementById("menuBtn").style.display = "flex";
+    document.getElementById("close").style.display = "none";
+}
+
